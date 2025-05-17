9a66d62d89543a31b5adaee524a580433fc89a7f
Maxim Shchibrik <partycle2006nt@gmail.com>
2025-05-17 16:07:15 +0500

    api basics

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from fastapi import FastAPI
+from enum import Enum
+import aiofiles
+import uuid
+
+from fastapi import FastAPI, UploadFile
+
+class Method(str, Enum):
+    SPECIES = 'species'
+    DISEASE = 'disease'
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+@app.post('/detect/{method}')
+async def detect(method: Method, img: UploadFile):
+    filepath = f'./images/{uuid.uuid4()}.jpeg'
+    async with aiofiles.open(filepath, 'wb') as out_file:
+        data = await img.read()
+        await out_file.write(data)
+
+    if method is Method.DISEASE:
+        return {'diagnosis': 'trachobobers', 'recommendations': '123'}
+    elif method is Method.SPECIES:
+        return {'russian_title': 'aboba', 'latin_title': 'bebra'}
+
+    return None
